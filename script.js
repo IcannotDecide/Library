8aabbe20961200360965a648ecfebab458d7013f
IcannotDecide <matthewat43@gmail.com>
2023-07-27 13:18:21 +0100

    Test js for adding books to arrays

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 let myLibrary = [];
 
 function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+};
+
+const book1 = new Book("All the bright places", "Jennifer", 200, "no");
+
+const book2 = new Book("Iron Man 3", "Bruce Lee", 200, "no");
+
+const book3 = new Book("Over the cuckoos nest", "Crazie", 200, "I don't read books");
+
+function addBookToLibrary(title, author, pages, status) {
   
 };
 
-function addBookToLibrary() {
+const cards = document.querySelector(".cards");
 
+myLibrary.push(book1, book2, book3);
+
+for (let i = 0; i < myLibrary.length; i++) {
+  cards.innerHTML = `<p>${myLibrary[i].title}`
 }
