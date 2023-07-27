109828eb87dab6663827108706a89264ef24f50d
IcannotDecide <matthewat43@gmail.com>
2023-07-27 16:05:21 +0100

    Start the add book form

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const cards = document.querySelector(".cards");
+
 let myLibrary = [];
 
 function Book(title, author, pages, status) {
@@ -13,26 +15,40 @@ const book2 = new Book("Iron Man 3", "Bruce Lee", 200, "no");
 
 const book3 = new Book("Over the cuckoos nest", "Crazie", 200, "I don't read books");
 
-function addBookToLibrary(title, author, pages, status) {
-  
-};
-
-const cards = document.querySelector(".cards");
-
 myLibrary.push(book1, book2, book3);
 
-for (let i = 0; i < myLibrary.length; i++) {
+function addBookToLibrary(title, author, pages, status) {
 
-  let newDiv = document.createElement("div");
-  newDiv.dataset.index = i;
-  let title = document.createElement("p");  
-  let author = document.createElement("p");
-  let pages = document.createElement("p");
-  let status = document.createElement("p");
-  title.innerText = myLibrary[i].title;
-  author.innerText = myLibrary[i].author;
-  pages.innerText = myLibrary[i].pages;
-  status.innerText = myLibrary[i].status;
-  newDiv.append(title, author, pages, status);
-  cards.append(newDiv);
+  // myLibrary.push(new Book(title, author, pages, status))
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.dataset.index = i;
+    let title = document.createElement("p");  
+    let author = document.createElement("p");
+    let pages = document.createElement("p");
+    let status = document.createElement("p");
+    title.innerText = myLibrary[i].title;
+    author.innerText = myLibrary[i].author;
+    pages.innerText = myLibrary[i].pages;
+    status.innerText = myLibrary[i].status;
+    newDiv.append(title, author, pages, status);
+    cards.append(newDiv);
+  }
+};
+
+addBookToLibrary();
+
+const button = document.querySelector("button");
+button.addEventListener("click", displayNone)
+
+const popup = document.querySelector(".popup");
+function displayNone() {
+    popup.style.display = "flex";
 }
+
+
+
+
+
+
